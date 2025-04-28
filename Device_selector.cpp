eac0fd60b48a073149e2b1ff2ee789106b43845f
Akash Bansode <119726870+akashbansode311@users.noreply.github.com>
2025-04-28 17:33:54 +0530

    Create Device_selector.cpp

---
+++ Device_selector.cpp
@@ -0,0 +1,18 @@
+#include <sycl/sycl.hpp>
+
+using namespace sycl;
+
+int main() {
+  //# Create a device queue with device selector
+  
+  queue q(gpu_selector_v);
+  //queue q(cpu_selector_v);
+  //queue q(accelerator_selector_v);
+  //queue q(default_selector_v);
+  //queue q;
+
+  //# Print the device name
+  std::cout << "Device: " << q.get_device().get_info<info::device::name>() << "\n";
+
+  return 0;
+}
